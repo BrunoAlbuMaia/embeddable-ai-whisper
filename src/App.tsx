4402487591a113c5f0f71b5fbe9005a48746dba2
BrunoAlbuMaia <brunoalbumaia18@gmail.com>
2025-08-12 23:01:04 -0300

    ajustando rotas e chat..

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { DashboardLayout } from "./components/Layout/DashboardLayout";
+
 import Index from "./pages/Index";
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
@@ -28,58 +29,83 @@ const App = () => {
         <Toaster />
         <Sonner />
         <ThemeProvider defaultTheme="system" storageKey="docs-ia-theme">
-          <AuthProvider>
-            <BrowserRouter>
+          <BrowserRouter>
             <Routes>
-              {/* Public routes */}
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<LoginPage />} />
+              {/* Rotas públicas */}
+              <Route path="/" element={ 
+                                        <AuthProvider>
+                                            <Index />
+                                        </AuthProvider>
+                                      } />
+              <Route path="/login" element=
+                                          {
+                                            <AuthProvider>
+                                              <LoginPage />
+                                            </AuthProvider>
+                                          }/>
               <Route path="/chat" element={<ChatPage />} />
-              
-              {/* Registration flow */}
               <Route path="/register" element={<SelectPlanPage />} />
               <Route path="/register/company" element={<RegisterCompanyPage />} />
               <Route path="/register/user" element={<RegisterUserPage />} />
 
-              {/* Protected dashboard routes */}
-              <Route path="/dashboard" element={
-                <ProtectedRoute>
-                  <DashboardLayout>
-                    <ChatsPage />
-                  </DashboardLayout>
-                </ProtectedRoute>
-              } />
-              <Route path="/dashboard/documents" element={
-                <ProtectedRoute>
-                  <DashboardLayout>
-                    <DocumentsPage />
-                  </DashboardLayout>
-                </ProtectedRoute>
-              } />
-              <Route path="/dashboard/segments" element={
-                <ProtectedRoute>
-                  <DashboardLayout>
-                    <SegmentsPage />
-                  </DashboardLayout>
-                </ProtectedRoute>
-              } />
-              <Route path="/dashboard/plans" element={
-                <ProtectedRoute>
-                  <DashboardLayout>
-                    <PlansPage />
-                  </DashboardLayout>
-                </ProtectedRoute>
-              } />
+              {/* Rotas protegidas */}
+              <Route
+                path="/dashboard"
+                element={
+                  <AuthProvider>
+                    <ProtectedRoute>
+                      <DashboardLayout>
+                        <ChatsPage />
+                      </DashboardLayout>
+                    </ProtectedRoute>
+                  </AuthProvider>
+                }
+              />
+              <Route
+                path="/dashboard/documents"
+                element={
+                  <AuthProvider>
+                    <ProtectedRoute>
+                      <DashboardLayout>
+                        <DocumentsPage />
+                      </DashboardLayout>
+                    </ProtectedRoute>
+                  </AuthProvider>
+                }
+              />
+              <Route
+                path="/dashboard/segments"
+                element={
+                  <AuthProvider>
+                    <ProtectedRoute>
+                      <DashboardLayout>
+                        <SegmentsPage />
+                      </DashboardLayout>
+                    </ProtectedRoute>
+                  </AuthProvider>
+                }
+              />
+              <Route
+                path="/dashboard/plans"
+                element={
+                  <AuthProvider>
+                    <ProtectedRoute>
+                      <DashboardLayout>
+                        <PlansPage />
+                      </DashboardLayout>
+                    </ProtectedRoute>
+                  </AuthProvider>
+                }
+              />
 
-              {/* Fallback routes */}
+              {/* 404 */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-            </BrowserRouter>
-          </AuthProvider>
+          </BrowserRouter>
         </ThemeProvider>
       </TooltipProvider>
     </QueryClientProvider>
   );
-}
+};
 
 export default App;
